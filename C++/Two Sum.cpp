97278b0594cb7d81e5c5d6590d907b674ac94f87
Muhammad Saad Amin <saadamin691@gmail.com>
2026-01-20 21:40:25 +0500

    123

---
+++ C++/Two Sum.cpp
@@ -0,0 +1,24 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> mp; // value -> index
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int complement = target - nums[i];
+            if (mp.find(complement) != mp.end())
+            {
+                return {mp[complement], i};
+            }
+            mp[nums[i]] = i;
+        }
+
+        return {}; // never reached as per problem constraint
+    }
+};
